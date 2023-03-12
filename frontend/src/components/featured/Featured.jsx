fb6c3608f99fbd22e52002df80b862e43c9fa22f
piyush wandile <piyushwandile134@gmail.com>
2023-03-12 16:51:31 +0530

    photos commit

---
+++ frontend/src/components/featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Featured.scss"
+
+const Featured = () => {
+  return (
+   <div className="featured">
+      <div className="conatiner">
+        <div className="left">
+            <h1>Find a perfect freelance services for your bussiness</h1>
+            <div className="search">
+                <div className="searchInput">
+                    <img src="./img/search.png" alt="" />
+                    <input type="text" placeholder="Try building a mobile app" />
+                </div>
+                <button>Sreach</button>
+            </div>
+            <div className="popular">
+                <span>Popular:</span>
+                <button>Web design</button>
+                <button>Wordpress</button>
+                <button>Logo design</button>
+                <button>Ai services</button>
+            </div>
+        </div>
+        <div className="rigth">
+            <img src="./img/man.png" alt="" />
+        </div>
+      </div>
+   </div>
+  )
+}
+
+export default Featured;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {Link, useLocation} from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
 
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+
+  const {pathname} = useLocation()
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -25,10 +28,12 @@ const Navbar = () => {
   }
 
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !=="/" ? "navbar active" : "navbar"}>
         <div className="container">
             <div className="logo">
+              <Link to="/">
               <span className='text'>Biverr</span>
+              </Link>
               <span className='dot'>.</span>
             </div>
         <div className="links">
@@ -45,30 +50,67 @@ const Navbar = () => {
                       {
                         currentUser?.isSeller && (
                           <>
-                            <span>Gigs</span>
-                            <span>Add New Gigs</span>
-                          </>
-                        )
-                      }
-                      <span>Orders</span>
-                      <span>Messages</span>
-                      <span>Logout</span>
-
+                          <Link className="link" to="/mygigs">
+                          Gigs
+                        </Link>
+                        <Link className="link" to="/add">
+                          Add New Gig
+                        </Link>
+                      </>
+                    )}
+                    <Link className="link" to="/orders">
+                      Orders
+                    </Link>
+                    <Link className="link" to="/messages">
+                      Messages
+                    </Link>
+                    <Link className="link" to="/">
+                      Logout
+                    </Link>
                     </div>}
                   </div>
                 )}
         </div>
       </div>
-      {active &&
-        <><hr/>
-         <div className='menu'>
-          <span>test 1</span>
-          <span>test 2</span>
-         </div>
+
+      
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
         </>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 
