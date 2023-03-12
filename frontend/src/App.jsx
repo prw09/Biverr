174cf81017370ccbac56263e0db4c3e035864d54
piyush wandile <piyushwandile134@gmail.com>
2023-03-12 12:02:22 +0530

    router commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,91 @@
-import React from "react"
-import Navbar from "./components/navbar/Navbar"
+import "./app.scss";
+import { 
+  createBrowserRouter, 
+  Outlet, 
+  RouterProvider }
+  from "react-router-dom";
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Add from "./pages/add/Add";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/MyGigs";
 
 function App() {
+
+  const Layout = ()=>{
+    return(
+      <>      
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+      </>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+      
+  
   
   return (
     <div className="App">
-     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda soluta tenetur fugit velit repudiandae ex impedit! Quas illo esse temporibus! Deserunt eligendi voluptates eum reiciendis alias iure unde excepturi inventore?
+     <RouterProvider router={router} />
     </div>
   )
 }
 
-export default App
+export default App;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Navbar.scss";
 
 const Navbar = () => {
+
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser={
+    id:1,
+    username:"Sayali",
+    isSeller:true
+
+  }
+
   return (
-    <div className="navbar">
+    <div className={active ? "navbar active" : "navbar"}>
         <div className="container">
             <div className="logo">
-                <span className='text'>Biverr</span>
-                <span className='dot'>.</span>
+              <span className='text'>Biverr</span>
+              <span className='dot'>.</span>
             </div>
         <div className="links">
                 <span>Biverr Business</span>
                 <span>Explore</span>
                 <span>English</span>
                 <span>Sign in</span>
-                <span>Become a Seller</span>
-                <button>Join</button>
+                {!currentUser?.isSeller && <span>Become a Seller</span>}
+                {!currentUser && <button>Join</button>}
+                {currentUser && (
+                  <div className="user" onClick={()=>setOpen(!open)}>
+                    <span>{currentUser?.username}</span>
+                   {open && <div className="options">
+                      {
+                        currentUser?.isSeller && (
+                          <>
+                            <span>Gigs</span>
+                            <span>Add New Gigs</span>
+                          </>
+                        )
+                      }
+                      <span>Orders</span>
+                      <span>Messages</span>
+                      <span>Logout</span>
+
+                    </div>}
+                  </div>
+                )}
         </div>
-        </div>
+      </div>
+      {active &&
+        <><hr/>
+         <div className='menu'>
+          <span>test 1</span>
+          <span>test 2</span>
+         </div>
+        </>
+      }
     </div>
   )
 }
